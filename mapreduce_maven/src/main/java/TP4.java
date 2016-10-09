df9d38f0dd22f05c4e6f9376a8d01dbd4bacaa52
Elmer Bayol <ebayol@emi.u-bordeaux1.fr>
2016-10-09 12:04:48 +0200

    ajout du code source pour l'exercice 4(compile mais je n'ai pas réussi à l'executer)

--- mapreduce_maven/src/main/java/TP4.java
+++ mapreduce_maven/src/main/java/TP4.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BooleanWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,22 +15,30 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class TP4 {
 
 	public static class MonProgMapper extends
-			Mapper<IntWritable, Point2DWritable, IntWritable, Point2DWritable> {
+			Mapper<IntWritable, Point2DWritable, NullWritable, BooleanWritable> {
 		public void map(IntWritable key, Point2DWritable value, Context context)
 				throws IOException, InterruptedException {
-			context.write(key, value);
+                    double x = value.getX();
+                    double y = value.getY();
+                    if((Math.pow(x, 2)+Math.pow(y, 2))<1){
+                        context.write(NullWritable.get(), new BooleanWritable(true));
+                    }
+			context.write(NullWritable.get(), new BooleanWritable(false));
 		}
 	}
 
 	public static class MonProgReducer extends
-			Reducer<IntWritable, Point2DWritable, IntWritable, Text> {
-		public void reduce(IntWritable key, Iterable<Point2DWritable> values,
+			Reducer<NullWritable, BooleanWritable, NullWritable, DoubleWritable> {
+		public void reduce(NullWritable key, Iterable<BooleanWritable> values,
 				Context context) throws IOException, InterruptedException {
-			String result = null;
-			for (Point2DWritable point2dWritable : values) {
-				result = point2dWritable.toString();
+                        int counterIn = 0;
+                        int counterTotal = 0;
+			for (BooleanWritable isInCircle : values) {
+                            if (isInCircle.get())
+                                counterIn++;
+                            counterTotal++;
 			}
-			context.write(key, new Text(result));
+			context.write(key, new DoubleWritable(4*(counterTotal/counterIn)));
 		}
 	}
 
@@ -39,11 +50,11 @@ public class TP4 {
 		job.setNumReduceTasks(1);
 		job.setJarByClass(TP4.class);
 		job.setMapperClass(MonProgMapper.class);
-		job.setMapOutputKeyClass(IntWritable.class);
-		job.setMapOutputValueClass(Point2DWritable.class);
+		job.setMapOutputKeyClass(NullWritable.class);
+		job.setMapOutputValueClass(BooleanWritable.class);
 		job.setReducerClass(MonProgReducer.class);
-		job.setOutputKeyClass(IntWritable.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(DoubleWritable.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 		job.setInputFormatClass(RandomPointInputFormat.class);
 		// FileInputFormat.addInputPath(job, new Path(args[0]));
